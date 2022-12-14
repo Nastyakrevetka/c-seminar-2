c3e81001b9969a105c3be42d9bd528730dd1d5e7
Nastya_krevtka <nastyamkala1278@gmail.com>
2022-12-14 22:48:34 +0300

    задача 2

---
+++ numb1/numb4/Program.cs
@@ -0,0 +1,15 @@
+﻿
+ Console.WriteLine("Введите первое число: ");
+ int chislo1 = int.Parse(Console.ReadLine());
+ Console.WriteLine("Введите второе число: ");
+ int chislo2 = int.Parse(Console.ReadLine());
+if (chislo2 % chislo1 == 0) 
+{
+Console.Write(chislo2);
+Console.Write(" кратно ");
+Console.Write(chislo1);
+}
+else {
+    Console.Write("остаток ");
+    Console.WriteLine(chislo2 % chislo1);
+}
