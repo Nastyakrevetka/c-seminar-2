7c2637c220196abe3aa4b6225d114e198ca6595b
Nastya_krevtka <nastyamkala1278@gmail.com>
2022-12-14 23:40:51 +0300

    задача 4

---
+++ numb1/numb6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число: ");
+ int chislo1 = int.Parse(Console.ReadLine());
+ Console.WriteLine("Введите второе число: ");
+ int chislo2 = int.Parse(Console.ReadLine());
+if (chislo2 == chislo1*chislo1) 
+{
+Console.Write($"{chislo1} {chislo2} -> да");
+}
+else 
+{
+    Console.Write($"{chislo1} {chislo2} -> нет");
+}
