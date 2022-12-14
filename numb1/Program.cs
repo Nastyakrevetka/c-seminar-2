07bd54c5f39c0378a5955fc606d084ba1c89220c
Nastya_krevtka <nastyamkala1278@gmail.com>
2022-12-14 20:55:45 +0300

    семинар 2 решение общей задачи

---
+++ numb1/Program.cs
@@ -0,0 +1,12 @@
+﻿int random_chislo = new Random().Next(10,100);
+Console.Write("Случайное число: ");
+Console.WriteLine(random_chislo);
+int a = random_chislo/ 10;
+int max = a;
+int b = random_chislo% 10;
+if ( b > a)
+{
+max = b;
+}
+Console.Write("Большая цифра: ");
+Console.WriteLine(max);
