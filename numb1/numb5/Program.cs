3729823ab08fbfda757e3ec29d80267d1534ed4c
Nastya_krevtka <nastyamkala1278@gmail.com>
2022-12-14 23:28:08 +0300

    задача 3

---
+++ numb1/numb5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int chislo1 = int.Parse(Console.ReadLine());
+if (chislo1 % 23 == 0 && chislo1 % 7 == 0)
+{
+ Console.Write($"{chislo1} -> да");  
+}
+else
+{
+Console.Write($"{chislo1} -> нет");
+}
